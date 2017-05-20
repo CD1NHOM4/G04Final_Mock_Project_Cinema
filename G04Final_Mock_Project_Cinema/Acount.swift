a24391417e6fe3769dd2a6ba238789fb410891fe
THANH <thanh@THANHs-Mac.local>
2017-05-20 16:12:23 +0700

    Xây dựng Modal cho User, code chức năng Login/Logout/Regsiter (Nguyen Thanh An)

---
+++ G04Final_Mock_Project_Cinema/Acount.swift
@@ -0,0 +1,9 @@
+//
+//  Acount.swift
+//  G04Final_Mock_Project_Cinema
+//
+//  Created by THANH on 5/20/17.
+//  Copyright © 2017 HCMUTE. All rights reserved.
+//
+
+import Foundation
---
+++ G04Final_Mock_Project_Cinema/UserProfile.swift
@@ -0,0 +1,39 @@
+//
+//  UserProfile.swift
+//  G04Final_Mock_Project_Cinema
+//
+//  Created by THANH on 5/20/17.
+//  Copyright © 2017 HCMUTE. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserProfile: NSObject {
+    var userid: String
+    var fullName: String
+    var email: String
+    var address: String
+    var score: Double
+    var password: String
+    var phone: String
+    var balance: Double
+    //Khởi tạo
+    init(userid: String, fullName: String, email: String, address: String, score: Double,
+         password: String, phone: String, balance: Double) {
+        
+        self.userid = userid
+        self.fullName = fullName
+        self.email = email
+        self.address = address
+        self.score = score
+        self.password = password
+        self.phone = phone
+        self.balance = balance
+    }
+    //
+    convenience override init() {
+        self.init(userid: "", fullName: "",email: "", address: "", score: 0, password: "",
+                  phone: "", balance: 0)
+    }
+}
