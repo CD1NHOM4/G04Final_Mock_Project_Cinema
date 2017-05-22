d9fb7aab3ff9d9fa547117856d5456717971005d
THANH <thanh@THANHs-Mac.local>
2017-05-22 16:24:18 +0700

    Hoàn thành chức năng login (Thanh An)

--- G04Final_Mock_Project_Cinema/LogInViewController.swift
+++ G04Final_Mock_Project_Cinema/LogInViewController.swift
@@ -7,29 +7,116 @@
 //
 
 import UIKit
+import Firebase
+import MBProgressHUD
 
-class LogInViewController: UIViewController {
-
+class LogInViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var txtfEmail: UITextField!
+    @IBOutlet weak var txtfPass: UITextField!
+    var thongbaoDangXuLi: MBProgressHUD!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+        let dismiss: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(LogInViewController.DismissKeyboard))
+        view.addGestureRecognizer(dismiss)
+        observerKeyboard()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //Hiện Progresss đang tải
+    func showProgress() {
+        thongbaoDangXuLi = MBProgressHUD.showAdded(to: self.view, animated: true)
+        thongbaoDangXuLi.mode = MBProgressHUDMode.indeterminate
+        thongbaoDangXuLi.label.text = " Đang tải..."
     }
-    */
-
+    //Ẩn Progress
+    func hideProgress() {
+        thongbaoDangXuLi.hide(animated: true)
+    }
+    //Xử lí sự kiện khi nhấn btnLogin
+    @IBAction func btnLogin_Act(_ sender: Any) {
+        let email: String = txtfEmail.text!
+        let password: String = txtfPass.text!
+        
+        if (email.isEmpty || password.isEmpty) {
+            showAlertDialog(message: "Hãy điền đầy đủ thông tin");
+        }
+        else {
+            if !(Validate.isValidEmail(testStr: email)) {
+                self.showAlertDialog(message: "Sai định dạng Email")
+            }
+            else {
+                
+                //Hiện progress
+                self.showProgress()
+                Auth.auth().signIn(withEmail: email, password: password.md5()) { (user, error) in
+                    
+                    //Ẩn progress
+                    self.hideProgress()
+                    if (error == nil) {
+                        let srcUserInfo = self.storyboard?.instantiateViewController(withIdentifier: "userProfileId") as! UserProfileViewController
+                        self.present(srcUserInfo, animated: true)
+                    }
+                    else {
+                        self.showAlertDialog(message: "Đăng nhập không thành công")
+                    }
+                }
+            }
+        }
+    }
+    
+    //Hiện hộp thoại cảnh báo
+    func showAlertDialog(message: String) {
+        let alertView = UIAlertController(title: "Thông Báo", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertView.addAction(action)
+        self.present(alertView, animated: true, completion: nil)
+    }
+    //Xử lí nút đóng btnClose
+    @IBAction func btnClose_Act(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: - Show, Hide Keyboard
+    //Focus, quay lại textField
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if txtfEmail.isEditing {
+            txtfPass.becomeFirstResponder()
+        } else {
+            txtfPass.resignFirstResponder()
+        }
+        return true
+    }
+    
+    fileprivate func observerKeyboard(){
+        NotificationCenter.default.addObserver(self, selector: #selector(LogInViewController.keyboardWillShow), name:NSNotification.Name.UIKeyboardWillShow, object: self.view.window)
+        NotificationCenter.default.addObserver(self, selector: #selector(LogInViewController.keyboardWillHide), name:NSNotification.Name.UIKeyboardWillHide, object: self.view.window)
+    }
+    
+    //DismissKeyboard
+    func DismissKeyboard(){
+        view.endEditing(true)
+    }
+    
+    //Hiện Keyboard
+    func keyboardWillShow(sender: NSNotification) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.view.frame = CGRect(x: 0, y: -70, width: self.view.frame.width, height: self.view.frame.height)
+        }, completion: nil)
+    }
+    
+    //Ẩn Keyboard
+    func keyboardWillHide(sender: NSNotification) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+        }, completion: nil)
+    }
 }
+
