3423840f60c5ef70d2ed183fa8bf2ba9227da1e9
THANH <thanh@THANHs-Mac.local>
2017-05-24 16:34:40 +0700

    Hoàn thành chức năng UserProfile và Thay đổi pass (Thanh An)

---
+++ G04Final_Mock_Project_Cinema/ChangePassViewController.swift
@@ -0,0 +1,174 @@
+//
+//  ChangePassViewController.swift
+//  G04Final_Mock_Project_Cinema
+//
+//  Created by THANH on 5/24/17.
+//  Copyright © 2017 HCMUTE. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import MBProgressHUD
+
+class ChangePassViewController: UIViewController {
+    
+    @IBOutlet weak var txtfOldPass: UITextField!
+    @IBOutlet weak var txtfNewPass: UITextField!
+    @IBOutlet weak var txtfConfirmPass: UITextField!
+    
+    var user: UserProfile! = nil
+    var thongbaoDangXuLi: MBProgressHUD! = nil
+    var rfDatabase: DatabaseReference!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Gọi tham chiếu đến database trên FireBase
+        rfDatabase = Database.database().reference()
+        
+        //Dismiss bàn phím
+        let dismiss: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(LogInViewController.DismissKeyboard))
+        view.addGestureRecognizer(dismiss)
+        //
+        observerKeyboard()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //1.Hàm xử lí nút Back được nhấn
+    @IBAction func btnBack_Act(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //2.hàm xử lí button btnChangePass được nhấn
+    @IBAction func btnChangePass_Act(_ sender: Any) {
+        var oldPass: String = txtfOldPass.text!
+        var newPass: String = txtfNewPass.text!
+        let confirmPass: String = txtfConfirmPass.text!
+        
+        //Kiểm tra người dùng đã nhập thông tin chưa
+        if (oldPass.isEmpty || newPass.isEmpty || confirmPass.isEmpty) {
+            showAlertDialog(message: "Bạn cần điền đầy đủ thông tin")
+        }
+        else {
+            var isAcount: Bool = true;
+            //Kiểm Tra độ dài Mật khẩu
+            if (oldPass.characters.count < 6 || newPass.characters.count < 6 || confirmPass.characters.count < 6){
+                isAcount = false;
+                showAlertDialog(message: "Mật khẩu phải có ít nhất 6 kí tự");
+                return;
+            }
+            
+            //Kiểm tra Pass hiện tại có đúng
+            let checkPass = user.password
+            if (checkPass != oldPass.md5()) {
+                isAcount = false
+                showAlertDialog(message: "Mật khẩu không hợp lệ");
+                return ;
+            }
+            
+            //Kiểm tra Pass mới có trùng với confirmPass
+            if (newPass != confirmPass) {
+                isAcount = false;
+                showAlertDialog(message: "Mật khẩu không trùng khớp");
+                return ;
+            }
+            
+            //Nếu thoả các điều kiện
+            if (isAcount) {
+                
+                //Hiện Progress đang xử lí
+                self.showProgress()
+                
+                //Gọi hàm Thay đổi Pass của thư viện FireBase
+                newPass = newPass.md5()
+                Auth.auth().currentUser?.updatePassword(to: String(newPass)) { (error) in
+                    
+                    //Ẩn Progress đang xử lí
+                    self.hideProgress()
+                    
+                    //Nếu không xảy ra lỗi
+                    if error == nil
+                    {
+                        //Cập nhật Pass mới
+                        let dataUpdatePass = ["password": newPass.md5()!];
+                        self.rfDatabase.child("Acount").child(self.getUid()).updateChildValues(dataUpdatePass)
+                        
+                        //Hiện Thông Báo thành công
+                        let alertView = UIAlertController(title: "Thông Báo", message: "Đổi mật khẩu thành công", preferredStyle: .alert)
+                        let action = UIAlertAction(title: "OK", style: .default, handler: { (action: UIAlertAction) in
+                            self.dismiss(animated: true, completion: nil)
+                        })
+                        
+                        alertView.addAction(action)
+                        self.present(alertView, animated: true, completion: nil)
+                    }
+                        
+                        //Nếu có lỗi,hiện Thông Báo đổi pass thất bại
+                    else
+                    {
+                        self.showAlertDialog(message: "Đổi mật khẩu thất bại")
+                    }
+                }
+            }
+        }
+    }
+    
+    //Hàm lấy ID của User, cung cấp bởi lớp Firebase
+    func getUid() -> String {
+        return (Auth.auth().currentUser?.uid)!;
+    }
+    
+    //Hàm hiện Progress nếu đang trong quá trình xử lí
+    func showProgress() {
+        thongbaoDangXuLi = MBProgressHUD.showAdded(to: self.view, animated: true)
+        thongbaoDangXuLi.mode = MBProgressHUDMode.indeterminate
+        thongbaoDangXuLi.label.text = "Đang tải..."
+    }
+    //Hàm ẩn Progress đang xử lí
+    func hideProgress() {
+        thongbaoDangXuLi.hide(animated: true)
+    }
+    
+    //MARK: - Hiện, Ẩn Keyboard
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        txtfOldPass.resignFirstResponder()
+        txtfConfirmPass.resignFirstResponder()
+        txtfNewPass.resignFirstResponder()
+        return true
+    }
+    //
+    fileprivate func observerKeyboard(){
+        NotificationCenter.default.addObserver(self, selector: #selector(LogInViewController.keyboardWillShow), name:NSNotification.Name.UIKeyboardWillShow, object: self.view.window)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(LogInViewController.keyboardWillHide), name:NSNotification.Name.UIKeyboardWillHide, object: self.view.window)
+    }
+    //Ẩn Bàn Phím
+    func DismissKeyboard(){
+        view.endEditing(true)
+    }
+    //Hiện bàn phím khi click vào TextField
+    func keyboardWillShow(sender: NSNotification) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.view.frame = CGRect(x: 0, y: -160, width: self.view.frame.width, height: self.view.frame.height)
+        }, completion: nil)
+    }
+    //Ẩn Bàn phím
+    func keyboardWillHide(sender: NSNotification) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+        }, completion: nil)
+    }
+    
+    //Hàm hiện hộp thoại cảnh báo
+    func showAlertDialog(message: String) {
+        let alertView = UIAlertController(title: "Thông Báo", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertView.addAction(action)
+        self.present(alertView, animated: true, completion: nil)
+    }
+}
--- G04Final_Mock_Project_Cinema/UserProfileViewController.swift
+++ G04Final_Mock_Project_Cinema/UserProfileViewController.swift
@@ -7,29 +7,92 @@
 //
 
 import UIKit
+import Firebase
+import MBProgressHUD
 
 class UserProfileViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var txtFFullName: UITextField!
+    @IBOutlet weak var txtFEmail: UITextField!
+    
+    @IBOutlet weak var txtFPhone: UITextField!
+    @IBOutlet weak var txtFAddress: UITextField!
+    
+    @IBOutlet weak var txtScore: UITextField!
+    
+    @IBOutlet weak var txtFBalance: UITextField!
+    var refDatabase: DatabaseReference!
+    var thongbaoDangXuLi: MBProgressHUD!
+    var userToMove: UserProfile!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        refDatabase = Database.database().reference()
+        
+        //load User profile
+        //Hiện  progress Đang xử lí
+        self.showProgress()
+        if let userid = Auth.auth().currentUser?.uid {
+            refDatabase.child("Acount").child(userid).observeSingleEvent(of: .value, with: { (snapshot) in
+                self.hideProgress()
+                if let user = snapshot.value as? [String: AnyObject] {
+                    let fullName = user["fullname"] as? String ?? ""
+                    let email = user["email"] as? String ?? ""
+                    let address = user["address"] as? String ?? ""
+                    let phone = user["phone"] as? String ?? ""
+                    let score = String(user["score"] as? Double ?? 0)
+                    let balance = String(user["balance"] as? Double ?? 0)
+                    self.txtFFullName.text! = fullName
+                    self.txtFEmail.text! = email
+                    self.txtFAddress.text! = address
+                    self.txtFPhone.text! = phone
+                    self.txtScore.text! = score
+                    self.txtFBalance.text! = balance
+                    self.userToMove = UserProfile.init(userid: userid, fullName: fullName, email: email, address: address, score: Double(score)!, password: user["password"] as? String ?? "" , phone: phone,balance: Double(balance)!)
+                    
+                }
+            }) { (error) in
+                print(error.localizedDescription)
+            }
+        }
     }
-
+    
+    //Sự kiện btnChangePass
+    @IBAction func btnChangePass_Act(_ sender: Any) {
+        let srcChangePass = self.storyboard?.instantiateViewController(withIdentifier: "changePassView") as! ChangePassViewController
+        srcChangePass.user = userToMove
+        self.present(srcChangePass, animated: true)
+    }
+    //Sự kiện khi nhấn button đăng xuất khỏi ứng dụng
+    @IBAction func btnLogOut(_ sender: Any) {
+        
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            navigationController?.popViewController(animated: true)
+            self.dismiss(animated: true)
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+    }
+    //Hàm hiện Tiến trình đang được xử lí
+    func showProgress() {
+        thongbaoDangXuLi = MBProgressHUD.showAdded(to: self.view, animated: true)
+        thongbaoDangXuLi.mode = MBProgressHUDMode.indeterminate
+        thongbaoDangXuLi.label.text = "Đang tải..."
+    }
+    
+    //Hàm ẩn Tiến trình đang được xử lí
+    func hideProgress() {
+        thongbaoDangXuLi.hide(animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
