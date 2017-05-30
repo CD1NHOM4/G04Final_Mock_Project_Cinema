1955a3a5fdce4c4c5d8ab8a3363a1aa0039e1732
THANH <thanh@THANHs-Mac.local>
2017-05-30 17:27:08 +0700

    Desin custom Cell/ Label, TextField @ThanhAn

--- G04Final_Mock_Project_Cinema/CustomTableViewCell.swift
+++ G04Final_Mock_Project_Cinema/CustomTableViewCell.swift
@@ -2,23 +2,35 @@
 //  CustomTableViewCell.swift
 //  G04Final_Mock_Project_Cinema
 //
-//  Created by THANH on 5/27/17.
+//  Created by THANH on 5/30/17.
 //  Copyright © 2017 HCMUTE. All rights reserved.
 //
 
 import UIKit
 
 class CustomTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var imgPoster: UIImageView!
+    @IBOutlet weak var txtContent: UILabel!
+    @IBOutlet weak var txtType: UILabel!
+    @IBOutlet weak var txtTitle: UILabel!
+    
+    //Cấu hình cho Cell (ẢNh , Tiêu đề, Nội dung)
+    func configWithCell(movieDetail: MovieDetail) {
+        //txtType.text! = movieDetail.type
+        txtTitle.text = movieDetail.movieName
+        txtContent.text! = movieDetail.overview
+        imgPoster.image = Downloader.downloadImageWithURL(movieDetail.posterUrl)
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
 }
---
+++ G04Final_Mock_Project_Cinema/CustomTextField.swift
@@ -0,0 +1,58 @@
+//
+//  CustomTextField.swift
+//  G04Final_Mock_Project_Cinema
+//
+//  Created by THANH on 5/30/17.
+//  Copyright © 2017 HCMUTE. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class LogInTextField: UITextField {
+    
+    //Set độ cong viền cho TextField
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    //Set ảnh bên trái cho TextField
+    @IBInspectable var leftImage: UIImage?{
+        didSet {
+            updateView()
+        }
+    }
+    //
+    @IBInspectable var leftPadding: CGFloat = 0{
+        didSet{
+            updateView()
+        }
+    }
+    //Hàm update View
+    func updateView(){
+        if let image = leftImage{
+            leftViewMode = .always
+            
+            let imageView = UIImageView(frame: CGRect(x: leftPadding, y: 0, width: 27, height: 27))
+            
+            imageView.image = image
+            imageView.tintColor = tintColor
+            
+            var width = leftPadding + 20
+            
+            if borderStyle == UITextBorderStyle.none || borderStyle ==  UITextBorderStyle.line {
+                width = width + 10
+            }
+            
+            let view = UIView(frame: CGRect(x: 0, y: 0, width: width, height: 20))
+            view.addSubview(imageView)
+            leftView = view
+        } else{
+            //Nếu Ảnh nil
+            leftViewMode = .never
+        }
+        //
+        attributedPlaceholder = NSAttributedString(string: placeholder != nil ? placeholder!: "", attributes: [NSForegroundColorAttributeName: tintColor])
+    }
+    
+}
