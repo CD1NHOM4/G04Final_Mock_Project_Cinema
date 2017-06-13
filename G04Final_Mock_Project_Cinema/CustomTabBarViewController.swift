9eee3e150c4b6784078eb6f99d9da5978ee075ae
THANH <thanh@THANHs-Mac.local>
2017-06-13 17:48:07 +0700

    Custom Layout cho Trang chu @ThanhAn

---
+++ G04Final_Mock_Project_Cinema/CustomTabBarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTabBarViewController.swift
+//  G04Final_Mock_Project_Cinema
+//
+//  Created by THANH on 6/13/17.
+//  Copyright © 2017 HCMUTE. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+    
+    @IBOutlet weak var financialTabBar: UITabBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // I've added this line to viewDidLoad
+        //UIApplication.shared.statusBarFrame.size.height
+        //        financialTabBar.frame = CGRect(x: 0, y:  financialTabBar.frame.size.height, width: financialTabBar.frame.size.width, height: financialTabBar.frame.size.height)
+    }
+}
