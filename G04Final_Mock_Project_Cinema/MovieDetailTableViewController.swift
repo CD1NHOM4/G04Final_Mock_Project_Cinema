10fbc2e536ea4ff503389a05d2b6b71e3f87d3d6
THANH <thanh@THANHs-Mac.local>
2017-06-22 17:57:43 +0700

    Code chức năng cho gia diện chi tiết phim @ThanhAn

---
+++ G04Final_Mock_Project_Cinema/MovieDetailTableViewController.swift
@@ -0,0 +1,74 @@
+//
+//  MovieDetailTableViewController.swift
+//  G04Final_Mock_Project_Cinema
+//
+//  Created by THANH on 6/21/17.
+//  Copyright © 2017 HCMUTE. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailTableViewController: UITableViewController {
+    var movieDetail: MovieDetail!
+    @IBOutlet weak var lbMovieTitle: UILabel!
+    @IBOutlet weak var lbGenres: UILabel!
+    @IBOutlet weak var lbFilmType: UILabel!
+    @IBOutlet weak var txtvOverView: UITextView!
+    
+    @IBOutlet weak var lbDirector: UILabel!
+    @IBOutlet weak var lbActor: UILabel!
+    @IBOutlet weak var lbReleaseDay: UILabel!
+    @IBOutlet weak var lbVoteAverage: UILabel!
+    @IBOutlet weak var imgPoster: UIImageView!
+    var type: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.clearsSelectionOnViewWillAppear = false
+        //Khởi tạo
+        initData()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    //load Data
+    func initData() {
+        lbMovieTitle.text = movieDetail.movieName
+        lbGenres.text = movieDetail.genres
+        txtvOverView.text = movieDetail.overview
+        lbDirector.text = movieDetail.director
+        lbActor.text = movieDetail.actor
+        lbReleaseDay.text = movieDetail.releaseDate
+        lbVoteAverage.text = String(movieDetail.voteAverage)
+        
+        if (movieDetail.movieType == "PhimDangChieu") {
+            type = "Đang chiếu"
+        }
+        else if (movieDetail.movieType == "PhimSapChieu") {
+            type = "Sắp chiếu"
+        }
+        else {
+            type = "Đã chiếu"
+        }
+        lbFilmType.text = type
+        imgPoster.image = Downloader.downloadImageWithURL(movieDetail.posterUrl)
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        if (movieDetail.movieType != "PhimDangChieu"){
+            return 1
+        }
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+    
+    //chon suat chieu
+}
